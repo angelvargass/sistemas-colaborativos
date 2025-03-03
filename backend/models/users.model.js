b5dad46680bc0341edfc71ca447391eb3be6fbea
FabsSWD <fabarcamonge@gmail.com>
2025-03-03 16:13:41 -0600

    fix: goofy git bug
    Just correcting random git bugs.

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,9 +1,4 @@
-<<<<<<< Updated upstream
-import mongoose from "mongoose";
-=======
-import mongoose from 'mongoo1se'
-import bcrypt from 'bcrypt'
->>>>>>> Stashed changes
+import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema(
   {
@@ -13,60 +8,22 @@ const userSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      required: true,
-<<<<<<< Updated upstream
-=======
-      unique: true // para evitar usuarios duplicados.
->>>>>>> Stashed changes
+      required: true
     },
     password: {
       type: String,
       required: false
     },
     phone: {
-<<<<<<< Updated upstream
-        type: String,
-        required: false,
-      },
-  },
-  {
-    timestamps: true, // createdAt, updatedAt
-=======
       type: String,
       required: false
-    },
-    // Manejo de roles y bloqueo
-    role: {
-      type: String,
-      default: 'user' // Puede ser "admin", "user", etc.
-    },
-    isBlocked: {
-      type: Boolean,
-      default: false
     }
   },
   {
-    timestamps: true // Crea automáticamente los campos createdAt y updatedAt.
->>>>>>> Stashed changes
+    timestamps: true // createdAt, updatedAt
   }
 )
 
-<<<<<<< Updated upstream
-const User = mongoose.model("User", userSchema);
-=======
-// Hook para hashear la contraseña antes de guardar
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next()
-  try {
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-    next()
-  } catch (error) {
-    next(error)
-  }
-})
-
 const User = mongoose.model('User', userSchema)
->>>>>>> Stashed changes
 
 export default User
