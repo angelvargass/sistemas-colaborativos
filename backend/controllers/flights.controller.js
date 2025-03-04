acf0958acbc0c13101fccb74983f0f2db3a3048c
angelvargass <59036972+angelvargass@users.noreply.github.com>
2025-03-03 20:45:29 -0600

    fix: remove ids mapping

--- backend/controllers/flights.controller.js
+++ backend/controllers/flights.controller.js
@@ -2,8 +2,7 @@ import flightsData from '../available_flights.json' assert { type: "json" };
 
 export const getFlights = async (req, res) => {
   try {
-    const flightsWithIds = flightsData.map((flight, index) => ({ id: index, ...flight }))
-    res.status(200).json({ success: true, data: flightsWithIds })
+    res.status(200).json({ success: true, data: flightsData })
   } catch (error) {
     console.error('Error retrieving flight data:', error.message)
     res.status(500).json({ success: false, message: 'Internal server error.' })
